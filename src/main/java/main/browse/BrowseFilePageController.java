97aeb782f37266f9113223fad5c835274320e8c4
BlakeGithub2 <BMiller3.1415@yahoo.com>
2021-05-15 09:32:04 -0700

    Minor refactors to BrowseFilePageController

--- src/main/java/main/browse/BrowseFilePageController.java
+++ src/main/java/main/browse/BrowseFilePageController.java
@@ -3,12 +3,14 @@ package main.browse;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.DirectoryChooser;
 import main.Main;
 import utils.FileUtils;
 
 import java.io.File;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class BrowseFilePageController implements Initializable {
@@ -27,10 +29,17 @@ public class BrowseFilePageController implements Initializable {
 
     @FXML
     private void add() {
+        File selectedDir = chooseDirectory();
+        showAddProjectConfirmation(selectedDir);
+    }
+    private File chooseDirectory() {
         DirectoryChooser chooser = new DirectoryChooser();
         chooser.setTitle("Select Project Directory");
         File selectedDir = chooser.showDialog(Main.getSceneController().getMainWindow());
 
+        return selectedDir;
+    }
+    private void showAddProjectConfirmation(File selectedDir) {
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
         alert.setTitle("Import Project Confirmation");
         alert.setHeaderText("Import project " + selectedDir.getName() + "?");
@@ -40,7 +49,21 @@ public class BrowseFilePageController implements Initializable {
         contentText = contentText.concat("Remaining disk storage: " + FileUtils.getByteValueString(selectedDir.getUsableSpace()) + "\n");
         alert.setContentText(contentText);
 
-        alert.show();
+        createAddProjectConfirmationAlertButtons(alert);
+    }
+    private void createAddProjectConfirmationAlertButtons(Alert alert) {
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == ButtonType.OK) {
+            addProject();
+        } else {
+            cancelAddProject();
+        }
+    }
+    private void addProject() {
+
+    }
+    private void cancelAddProject() {
+
     }
 
     @Override
