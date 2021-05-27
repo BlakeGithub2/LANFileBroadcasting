057f1bbb9771fc3e31e230af9a8f1e414fbaffda
BlakeGithub2 <BMiller3.1415@yahoo.com>
2021-05-27 00:48:25 -0700

    Add project unit tests still in progress

--- src/test/java/main/browse/BrowseFilePageControllerTest.java
+++ src/test/java/main/browse/BrowseFilePageControllerTest.java
@@ -7,6 +7,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.testfx.util.WaitForAsyncUtils;
 
+import java.io.File;
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BrowseFilePageControllerTest extends BaseFileUnitTest {
@@ -31,21 +34,54 @@ public class BrowseFilePageControllerTest extends BaseFileUnitTest {
     }
 
     @Test
-    public void testAddProject() {
+    public void testAddNonexistentProject() {
         clickOn(lookup("#addButton").queryButton());
+
         press(KeyCode.N);
         press(KeyCode.ENTER);
-        try {
-            Thread.sleep(100);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+
+        WaitForAsyncUtils.waitForFxEvents();
+        // TODO: Check alert text
+
+        press(KeyCode.ENTER);
+        press(KeyCode.ESCAPE);
+
+        assertEquals(0, lookup("#projectList").queryListView().getItems().size());
+    }
+
+    @Test
+    public void testAddProject() throws IOException {
+        // TODO: Same code in BrowseFilePageTest. Refactor?
+        File newFile = new File(baseFolder + "/test");
+        newFile.createNewFile();
+
+        // Add the project
+        clickOn(lookup("#addButton").queryButton());
+
+        inputText(baseFolder.getAbsoluteFile() + "/test");
+        press(KeyCode.ENTER);
+
+        newFile.delete();
+    }
+    private void inputText(String text) {
+        text = text.toUpperCase();
+        for (int i = 0; i < text.length(); i++) {
+            char c = text.charAt(i);
+            System.out.println(c);
+            if (c == ':') {
+                press(KeyCode.COLON);
+            } else if (c == '\\') {
+                press(KeyCode.BACK_SLASH);
+            } else {
+                press(KeyCode.getKeyCode(c + ""));
+            }
         }
-        //clickOn(lookup("#nonexistentFileAlertOkButton").queryButton());
     }
 
     @BeforeEach
     public void beforeEach() throws Exception {
         super.beforeEach();
+
         Platform.runLater(() -> {
             controller = new BrowseFilePageController();
             Main.getSceneController().activate("browse");
